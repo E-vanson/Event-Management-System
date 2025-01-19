fbdc9d7afa7b5c36e199de1f6e400b9420f39680
Evans <evansonkamau233@gmail.com>
2025-01-19 18:35:32 +0300

    Event Module Initialized

---
+++ Backend/models/eventsModel.js
@@ -0,0 +1,12 @@
+import { DataTypes } from "sequelize";
+import { sequelizer } from "../config/db.js";
+
+
+const Event = sequelizer.define(
+    "Event",
+    {
+
+    }
+)
+
+export default Event;
